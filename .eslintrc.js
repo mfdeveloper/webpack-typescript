59d7857614f12b4ad957c0399060e946bf8b19da
Felipe <mfelipeof@gmail.com>
2021-06-28 10:47:50 +0100

    chore(debug): Enable vscode ts debug on chrome browser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,63 @@
 const orderingRule = require('@typescript-eslint/eslint-plugin/dist/rules/member-ordering');
 
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['tsconfig.json'],
-  },
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  plugins: ['@typescript-eslint', 'jest'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-  ],
-  overrides: [
-    {
-      files: ['**/*.spec.ts'],
-      rules: {
-        '@typescript-eslint/unbound-method': 'off',
-        'jest/unbound-method': 'error',
-      },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['tsconfig.json'],
     },
-  ],
-  rules: {
-    'prettier/prettier': [
-      'warn',
-      {
-        endOfLine: 'auto',
-      },
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    plugins: ['@typescript-eslint', 'jest'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
     ],
-    '@typescript-eslint/explicit-member-accessibility': 'warn',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/member-ordering': [
-      'warn',
-      {
-        default: {
-          memberTypes: orderingRule.defaultOrder,
-          order: 'as-written',
+    overrides: [
+        {
+            files: ['**/*.spec.ts'],
+            rules: {
+                '@typescript-eslint/unbound-method': 'off',
+                'jest/unbound-method': 'error',
+            },
         },
-      },
     ],
-    '@typescript-eslint/unbound-method': 'error',
-  },
+    rules: {
+        'prettier/prettier': [
+            'warn',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/unbound-method': 'error',
+        '@typescript-eslint/member-ordering': [
+            'warn',
+            {
+                default: {
+                    memberTypes: orderingRule.defaultOrder,
+                    order: 'as-written',
+                },
+            },
+        ],
+        '@typescript-eslint/explicit-member-accessibility': [
+            'warn',
+            {
+                accessibility: 'explicit',
+                overrides: {
+                  accessors: 'explicit',
+                  constructors: 'no-public',
+                  methods: 'no-public',
+                  properties: 'no-public',
+                  parameterProperties: 'explicit'
+                }
+            }
+        ],
+    },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -5,104 +5,109 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const { merge } = require('webpack-merge');
+let exclude = [path.resolve(__dirname, 'dist')];
 
 const base = {
-  entry: {
-    'webpack-typescript': ['scripts/app.ts'],
-  },
-  context: path.resolve(__dirname, 'src'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'scripts/[name].[chunkhash:8].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: { onlyCompileBundledFiles: true },
-          },
+    entry: {
+        'webpack-typescript': ['scripts/app.ts'],
+    },
+    context: path.resolve(__dirname, 'src'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'scripts/[name].[chunkhash:8].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: { onlyCompileBundledFiles: true },
+                    },
+                ],
+            },
         ],
-      },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: 'body',
+            scriptLoading: 'defer',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'public' }],
+            options: {
+                concurrency: 100,
+            },
+        }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      inject: 'body',
-      scriptLoading: 'defer',
-    }),
-    new CopyPlugin({
-      patterns: [{ from: 'public' }],
-      options: {
-        concurrency: 100,
-      },
-    }),
-  ],
-  resolve: {
-    modules: ['node_modules', path.resolve(__dirname, 'src')],
-    extensions: ['.ts', '.js', 'scss'],
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
+    resolve: {
+        modules: ['node_modules', path.resolve(__dirname, 'src')],
+        extensions: ['.ts', '.js', '.scss'],
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
 };
 
 const prod = {
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    output: {
+        publicPath: '/',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[chunkhash:8].css',
+            chunkFilename: 'css/[id].[chunkhash:8].css',
+        }),
     ],
-  },
-  output: {
-    publicPath: '/',
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[chunkhash:8].css',
-      chunkFilename: 'css/[id].[chunkhash:8].css',
-    }),
-  ],
 };
 
 const dev = {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  output: {
-    publicPath: 'http://localhost:8080/',
-  },
-  stats: {
-    colors: true,
-  },
-  devServer: {
-    contentBase: './dist',
-    clientLogLevel: 'info',
-    port: 8080,
-    inline: true,
-    historyApiFallback: false,
-    hot: true,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 500,
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
     },
-  },
-  devtool: 'inline-source-map',
+    output: {
+        publicPath: 'http://localhost:8080/',
+    },
+    stats: {
+        colors: true,
+    },
+    devServer: {
+        contentBase: './dist',
+        clientLogLevel: 'info',
+        port: 8080,
+        inline: true,
+        historyApiFallback: false,
+        hot: true,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 500,
+        },
+    },
+    devtool: 'source-map',
 };
 
-module.exports = (env) => merge(base, env && env.production ? prod : dev);
+module.exports = (env) =>
+    merge(
+        base,
+        (env && env.production) || process.env.NODE_ENV == 'production' ? prod : dev
+    );
